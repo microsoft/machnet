443b0c9056fd2ad78e096b06172e40ccb91775ae
Ilias Marinos <ilias@marinos.io>
2023-10-04 22:31:04 +0000

    Minor fixes and restructuring in `jring_bench`.

--- src/ext/jring_bench.cc
+++ src/ext/jring_bench.cc
@@ -9,37 +9,39 @@ static constexpr size_t kProducerCore = 2;
 static constexpr size_t kConsumerCore = 5;
 
 static constexpr size_t kQueueSz = 1024;
-static constexpr size_t kWindowSize = 16;
-
-static constexpr size_t kMsgSize = 64;
+static constexpr size_t KMaxMsgSize = 2048;
 
 class ProducerConsumerBenchmark : public benchmark::Fixture {
  public:
   ProducerConsumerBenchmark()
       : p2c_ring_(nullptr), c2p_ring_(nullptr), stop_(false) {}
-  // This is the nconsume thread function.
+  /**
+   * @brief Consumer task.
+   */
   void consumer_task() {
     juggler::utils::BindThisThreadToCore(kConsumerCore);
 
-    std::vector<uint8_t> buf(kMsgSize, 'k');
-    std::vector<uint8_t> expected_buf(kMsgSize, 'a');
+    std::vector<uint8_t> buf(KMaxMsgSize);
+    auto nb_rx = 0u;
     while (!stop_.load()) {
-      jring2_dequeue_burst(p2c_ring_, buf.data(), 1);
+      nb_rx += jring2_dequeue_burst(p2c_ring_, buf.data(), 1);
     }
+    (void)nb_rx;
   }
 
   void SetUp(const ::benchmark::State& state) override {
-    // nInitialization code here...
-    const auto kRingMemSz = jring2_get_buf_ring_size(kMsgSize, kQueueSz);
+    // Initialize the message size from the benchmark state.
+    const size_t msg_size = static_cast<size_t>(state.range(0));
+    const auto ring_mem_size = jring2_get_buf_ring_size(msg_size, kQueueSz);
 
     // Allocate memory for the rings.
     p2c_ring_ = CHECK_NOTNULL(
-        static_cast<jring2_t*>(aligned_alloc(CACHELINE_SIZE, kRingMemSz)));
-    jring2_init(p2c_ring_, kQueueSz, kMsgSize);
+        static_cast<jring2_t*>(aligned_alloc(CACHELINE_SIZE, ring_mem_size)));
+    jring2_init(p2c_ring_, kQueueSz, msg_size);
 
     c2p_ring_ = CHECK_NOTNULL(
-        static_cast<jring2_t*>(aligned_alloc(CACHELINE_SIZE, kRingMemSz)));
-    jring2_init(c2p_ring_, kQueueSz, kMsgSize);
+        static_cast<jring2_t*>(aligned_alloc(CACHELINE_SIZE, ring_mem_size)));
+    jring2_init(c2p_ring_, kQueueSz, msg_size);
 
     // Start the consumer thread.
     stop_.store(false);
@@ -64,28 +66,38 @@ class ProducerConsumerBenchmark : public benchmark::Fixture {
 
 BENCHMARK_DEFINE_F(ProducerConsumerBenchmark, ProducerBenchmark)
 (benchmark::State& st) {  // NOLINT
-  // Give it a moment to start
-  // Pause the timer to exclude the sleep from measurements
-  st.PauseTiming();
-  const uint32_t num_messages = st.range(0);
+  const size_t msg_size = static_cast<size_t>(st.range(0));
+  const uint32_t num_messages = static_cast<uint32_t>(st.range(1));
+  CHECK_GE(msg_size, sizeof(uint64_t));
+
   juggler::utils::BindThisThreadToCore(kProducerCore);
   sleep(2);
-  st.ResumeTiming();
 
-  std::vector<uint8_t> tx_buf(kMsgSize, 'a');
+  std::vector<uint8_t> tx_buf(msg_size, 'a');
   auto nb_tx = 0u;
   for (auto _ : st) {
-    while (nb_tx < num_messages) {
-      nb_tx += jring2_enqueue_bulk(p2c_ring_, tx_buf.data(), 1);
+    auto iteration_nb_tx = 0u;
+    while (iteration_nb_tx < num_messages) {
+      ::benchmark::DoNotOptimize(tx_buf);
+      ::benchmark::DoNotOptimize(
+          iteration_nb_tx += jring2_enqueue_bulk(p2c_ring_, tx_buf.data(), 1));
     }
+    nb_tx += iteration_nb_tx;
   }
+
   st.counters["msg_rate"] =
       benchmark::Counter(nb_tx, benchmark::Counter::kIsRate);
   st.counters["bps"] =
-      benchmark::Counter(nb_tx * kMsgSize, benchmark::Counter::kIsRate);
+      benchmark::Counter(nb_tx * msg_size * 8, benchmark::Counter::kIsRate);
 }
 
 BENCHMARK_REGISTER_F(ProducerConsumerBenchmark, ProducerBenchmark)
-    ->Range(1 << 20, 1 << 28);  // [1M, 16M]
+    ->Args({64, 1 << 24})    // msg_size = 64, num_messages = 16M
+    ->Args({128, 1 << 24})   // msg_size = 128, num_messages = 16M
+    ->Args({256, 1 << 24})   // msg_size = 256, num_messages = 16M
+    ->Args({512, 1 << 24})   // msg_size = 512, num_messages = 16M
+    ->Args({1024, 1 << 24})  // msg_size = 1024, num_messages = 16M
+    ->Args({2048, 1 << 24})  // msg_size = 1024, num_messages = 16M
+    ->Iterations(10);  // number of iterations for each case
 
 BENCHMARK_MAIN();
