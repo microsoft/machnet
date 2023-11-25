e40e1bf1e45baf87c8d6001c4444e3a9effd65e6
vjabrayilov <vahab.jabrayilov@metu.edu.tr>
2023-11-25 16:51:36 -0500

    pprof at store logs to see where other thread blocked

--- go/raft/store.go
+++ go/raft/store.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"encoding/binary"
 	"github.com/golang/glog"
+	"os"
+	"runtime/pprof"
 	"time"
 
 	"github.com/hashicorp/go-msgpack/codec"
@@ -101,6 +103,7 @@ func (s *BuffStore) StoreLog(log *raft.Log) error {
 func (s *BuffStore) StoreLogs(logs []*raft.Log) error {
 	start := time.Now()
 	glog.Warningf("StoreLogs: started at %+v", start)
+	pprof.Lookup("goroutine").WriteTo(os.Stderr, 1)
 	for _, log := range logs {
 		if err := s.StoreLog(log); err != nil {
 			return err
