c1d66876cf09d7ce7321fd57ece18d08cdb45f30
vjabrayilov <vahab.jabrayilov@metu.edu.tr>
2023-11-26 22:11:47 -0500

    remove redundant logging

--- go/raft/store.go
+++ go/raft/store.go
@@ -3,11 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-	"github.com/golang/glog"
-	"os"
-	"runtime/pprof"
-	"time"
-
 	"github.com/hashicorp/go-msgpack/codec"
 	"github.com/hashicorp/raft"
 )
@@ -101,15 +96,11 @@ func (s *BuffStore) StoreLog(log *raft.Log) error {
 }
 
 func (s *BuffStore) StoreLogs(logs []*raft.Log) error {
-	start := time.Now()
-	glog.Warningf("StoreLogs: started at %+v", start)
-	pprof.Lookup("goroutine").WriteTo(os.Stderr, 1)
 	for _, log := range logs {
 		if err := s.StoreLog(log); err != nil {
 			return err
 		}
 	}
-	glog.Warningf("StoreLogs: took %+v", time.Since(start))
 	return nil
 }
 
