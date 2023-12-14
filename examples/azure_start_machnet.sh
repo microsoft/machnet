a1534e878ef168de4303d242b3067018ffbb8811
ankalia <ankalia@microsoft.com>
2023-12-14 03:24:52 +0000

    Allow azure_start_machnet.sh to be run from any directory

--- examples/azure_start_machnet.sh
+++ examples/azure_start_machnet.sh
@@ -13,7 +13,7 @@ machnet_ip_addr=$(
     "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | \
     jq '.network.interface[1].ipv4.ipAddress[0].privateIpAddress' | \
     tr -d '"')
-echo "Machnet IP address: $machnet_ip_addr"
+echo "Machnet IP address: ${machnet_ip_addr}"
 
 machnet_mac_addr=$(
     curl -s -H Metadata:true --noproxy "*" \
@@ -21,7 +21,7 @@ machnet_mac_addr=$(
     jq '.network.interface[1].macAddress' | \
     tr -d '"' | \
     sed 's/\(..\)/\1:/g;s/:$//') # Converts AABBCCDDEEFF to AA:BB:CC:DD:EE:FF
-echo "Machnet MAC address: $machnet_mac_addr"
+echo "Machnet MAC address: ${machnet_mac_addr}"
 
 sudo modprobe uio_hv_generic
 
@@ -31,4 +31,5 @@ if [ -d /sys/class/net/eth1 ]; then
     sudo driverctl -b vmbus set-override $DEV_UUID uio_hv_generic
 fi
 
-cd ..; ./machnet.sh ${BARE_METAL_ARG} --mac $machnet_mac_addr --ip $machnet_ip_addr
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd ${THIS_SCRIPT_DIR}/..; ./machnet.sh ${BARE_METAL_ARG} --mac ${machnet_mac_addr} --ip ${machnet_ip_addr}
