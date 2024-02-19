e444e2947c6dabbce1001b11b33e784299bf2603
vjabrayilov <vahab.jabryailov@metu.edu.tr>
2024-02-19 00:33:34 +0000

    make bindings concise
    only generate Machnet API
    
    Signed-off-by: vjabrayilov <vahab.jabryailov@metu.edu.tr>

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,15 +1,15 @@
 use std::{env, path::PathBuf};
-
 fn main() {
-
     let lib_path = "../..";
 
-    println!("cargo:rustc-link-search=native={}",lib_path);
+    println!("cargo:rustc-link-search=native={}", lib_path);
     println!("cargo:rustc-link-lib=machnet_shim");
+    // construct a string
 
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+        .header(format!("{}/src/ext/machnet.h", lib_path))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .allowlist_function(".*machnet.*")
         .generate()
         .expect("Unable to generate bindings");
 
